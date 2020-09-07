84dada3f1a1aa5fe0ebe74f70cfc6b77c7e65024
Shadow <2724476411@qq.com>
2020-09-07 17:11:42 +0800

    UserServiceImpl.java接口方法实现

--- src/main/java/service/impl/UserServiceImpl.java
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -1,24 +1,32 @@
 package service.impl;
 
+import dao.UserDao;
 import entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import service.UserService;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
     public User getUserById(int id) {
-        return null;
+        return userDao.getUserById(id);
     }
 
     public List<User> getAllUser() {
-        return null;
+        return userDao.getUsers();
     }
 
     public boolean updateUser(User user) {
-        return false;
+        return userDao.updateUser(user);
     }
 
     public boolean delUser(int id) {
-        return false;
+        return userDao.delUser(id);
     }
 }
