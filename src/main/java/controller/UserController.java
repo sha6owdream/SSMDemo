a34ad12095674463bc0a726b5cf06154dcbebc9b
Shadow <2724476411@qq.com>
2020-09-07 17:21:40 +0800

    UserController 方法实现

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -4,7 +4,6 @@ import entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import service.UserService;
 
 import java.util.List;
