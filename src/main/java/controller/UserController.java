d6ce8aef1c75f6981df5e61713a3178e53c44bfe
Shadow <2724476411@qq.com>
2020-09-07 17:18:43 +0800

    UserController接口方法实现

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,4 +1,41 @@
 package controller;
 
+import entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import service.UserService;
+
+import java.util.List;
+
+@Controller
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public User getUserById(int id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUser();
+    }
+
+    @GetMapping
+    public boolean updateUser() {
+        User user = new User();
+        user.setName("Jim");
+        user.setUsername("root");
+        user.setPassword("123456");
+        return userService.updateUser(user);
+    }
+
+    @GetMapping
+    public boolean delUser(int id) {
+        return userService.delUser(id);
+    }
+
 }
