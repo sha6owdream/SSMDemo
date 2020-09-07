25094f65d76e4248fde2bf632075449602fb6a61
Shadow <2724476411@qq.com>
2020-09-07 16:59:19 +0800

    UserDao.java接口方法实现

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -2,9 +2,13 @@ package dao;
 
 
 import entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
+@Mapper
 public interface UserDao {
 
     /**
@@ -12,12 +16,14 @@ public interface UserDao {
      * @param id 要查找的用户id
      * @return 查找到的用户信息
      */
+    @Select("select * from user where id = #{id}")
     User getUserById(int id);
 
     /**
      * 查找所有用户
      * @return 用户集合
      */
+    @Select("select * from user")
     List<User> getUsers();
 
     /**
@@ -25,6 +31,7 @@ public interface UserDao {
      * @param user  要更新的用户信息
      * @return  更新是否成功
      */
+    @Update("Update user set name = #{name} username = #{username}, password = #{password} where id = #{id}")
     boolean updateUser(User user);
 
     /**
@@ -32,6 +39,7 @@ public interface UserDao {
      * @param id  要删除的用户id
      * @return 删除是否成功
      */
+    @Update("update User set deleted where id = #{id}")
     boolean delUser(int id);
 
 }
--- src/main/java/service/impl/UserServiceImpl.java
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -1,4 +1,24 @@
 package service.impl;
 
-public class UserServiceImpl {
+import entity.User;
+import service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    public User getUserById(int id) {
+        return null;
+    }
+
+    public List<User> getAllUser() {
+        return null;
+    }
+
+    public boolean updateUser(User user) {
+        return false;
+    }
+
+    public boolean delUser(int id) {
+        return false;
+    }
 }
